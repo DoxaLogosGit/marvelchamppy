b41a05e76761516b5bde6c30b5fdaec0d5d00cbf
Jay Atkinson <jgatkinsn@gmail.com>
2023-04-25 22:28:04 -0500

    speed up download by only downloading marvel champions data

--- retrieve_data.py
+++ retrieve_data.py
@@ -9,9 +9,11 @@ BASE_URL='https://www.boardgamegeek.com/xmlapi2/'
 PLAYS='plays?'
 ENTRIES_PER_PAGE=100
 
+MARVEL_CHAMPIONS_ID="285774"
+
 def determine_number_of_pages(xml_data):
     """
-    given the first play of play data, deterimne how may pages must be retrieved
+    given the first play of play data, determine how may pages must be retrieved
     note: 100 entries per page
     """
     root = ElementTree.fromstring(xml_data)
@@ -25,9 +27,9 @@ def retrieve_play_page(url, user, page_num='1'):
     Retrieves the play page given the page number
     returns the xml text
     """
-    #print("Retrieving page data...")
-    res = requests.get(url+PLAYS+"username="+user+"&type=family&page="+page_num)
-    #print(res)
+    print("Retrieving page data...")
+    res = requests.get(url+PLAYS+"username="+user+"&id="+MARVEL_CHAMPIONS_ID+"&type=thing&subtype=boardgame&page="+page_num)
+    print(res)
     if (res.status_code != 200):
         return ""
     else:
@@ -44,7 +46,7 @@ def retrieve_play_data_from_bgg(user):
         return -1
 
     pages = determine_number_of_pages(page_data)
-    #print("Pages of data to retrieve {}".format(pages))
+    print("Pages of data to retrieve {}".format(pages))
 
     xml_data.append(page_data)
     #pages = 2 #iterate fast for now
