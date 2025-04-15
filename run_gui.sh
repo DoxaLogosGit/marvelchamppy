87913df375742cb4e0da4375d6e1b79880948801
Jay Atkinson <jgatkinsn@gmail.com>
2025-04-14 22:11:53 -0500

    making easier to manage

--- run_gui.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-rm -rf __pycache__
-rm -rf widgets/__pycache__
-python3 mc_stat_gui.py
--- run_weekly_marvel_champs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#
-
-cd /home/jgatkinsn/projects/marvelchampy
-. pydev/bin/activate
-python3 marvel_champions_data.py -u
-
